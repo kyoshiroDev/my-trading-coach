7880d992200f5f0e9fa960ed4bff138dfd9f5cb2
Gregory Tahir <tahir.gregory.dev@gmail.com>
2026-04-06 19:37:12 +0200

    fix(analytics): empty error callbacks trigger lint error

--- apps/app-mytradingcoach/src/app/features/analytics/analytics.component.ts
+++ apps/app-mytradingcoach/src/app/features/analytics/analytics.component.ts
@@ -69,7 +69,7 @@ export class AnalyticsComponent implements OnInit {
   ngOnInit(): void {
     this.analyticsApi.getSummary().subscribe({
       next: (res) => this.summary.set(res.data),
-      error: () => {},
+      error: (_e: unknown) => { /* silently ignore */ },
     });
 
     if (this.userStore.isPremium()) {
@@ -80,19 +80,19 @@ export class AnalyticsComponent implements OnInit {
   private loadPremiumData(): void {
     this.analyticsApi.getEquityCurve().subscribe({
       next: (res) => this.equityCurve.set(res.data),
-      error: () => {},
+      error: (_e: unknown) => { /* silently ignore */ },
     });
     this.analyticsApi.getByHour().subscribe({
       next: (res) => this.heatmapData.set(res.data),
-      error: () => {},
+      error: (_e: unknown) => { /* silently ignore */ },
     });
     this.analyticsApi.getTopAssets().subscribe({
       next: (res) => this.topAssets.set(res.data),
-      error: () => {},
+      error: (_e: unknown) => { /* silently ignore */ },
     });
     this.analyticsApi.getBySetup().subscribe({
       next: (res) => this.setupData.set(res.data),
-      error: () => {},
+      error: (_e: unknown) => { /* silently ignore */ },
     });
   }
 
