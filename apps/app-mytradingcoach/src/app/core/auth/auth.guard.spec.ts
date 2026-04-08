844644449c417afc3e9c378604037e1bb5bdede8
Gregory Tahir <tahir.gregory.dev@gmail.com>
2026-04-08 21:21:47 +0200

    feat(stripe): mise en place du systeme de paiement stripe ok en environnement de dev

--- apps/app-mytradingcoach/src/app/core/auth/auth.guard.spec.ts
+++ apps/app-mytradingcoach/src/app/core/auth/auth.guard.spec.ts
@@ -66,7 +66,7 @@ describe('premiumGuard', () => {
 
     TestBed.runInInjectionContext(() => premiumGuard({} as any, {} as any));
 
-    expect(navigateSpy).toHaveBeenCalledWith(['/dashboard']);
+    expect(navigateSpy).toHaveBeenCalledWith(['/setting']);
   });
 
   it('retourne false si non premium', () => {
